51977698d98026bc17b841bcdd41c42a1a7b9ac8
RairaA <rairamathias@gmail.com>
2023-08-03 23:20:22 -0300

    criação do projeto

--- sistema de estoque.py
+++ sistema de estoque.py
@@ -1,7 +1,5 @@
 #importar o tkinter 
 import tkinter as tk
-login_e=''
-senha_e=''
 
 #criar a janela inicial
 janela=tk.Tk()
@@ -10,59 +8,17 @@ janela.geometry('300x400')
 
 #criar a def home()
 def home():
-    print("""  
-          [1]estoque
-          [2]vendas
-          [3]configuraçoes
-          [4]contatos
-         """)
-        
-    escolha=input('digite a escolha')
-    if escolha =='1':
-        estoque()
-    if escolha =='2':
-        vendas()
-    if escolha =='3':
-        config()
-    if escolha =='4':
-        contatos()
-
+    print('home')
 
 #criar def main pra colocar todas as def dentro()
 def main():
     print('*'* 30)
     print('P.O.G.E.R.S')
     print('*'* 30)
-    login()
-#criar a def pegar login():
-def pegar_login():
-    global login_e , senha_e
-    login=login_e.get()
-    senha=senha_e.get()
-    print(login)
-    print(senha)
-    janela.destroy()
 
 #criar def login()
 def login():
-
-    login_l=tk.Label(janela,text='login' )
-    login_l.pack()
-
-    login_e=tk.Entry(janela)
-    login_e.pack()
-
-    senha_l=tk.Label(janela,text='senha' )
-    senha_l.pack()
-
-    senha_e=tk.Entry(janela)
-    senha_e.pack()
-
-    botao=tk.Button(janela, text="ENVIAR", command=pegar_login, borderwidth=0)
-    botao.pack()
-
-    
-
+    print('login')
 
 #criar def estoque ()
 def estoque():
@@ -84,5 +40,5 @@ def contatos():
     print('contatos')
 
 
-main()
+
 janela.mainloop()
