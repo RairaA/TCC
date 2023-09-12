b72efc477e9a320a6a8e8e7e2da8586a4aa5273b
RairaA <rairamathias@gmail.com>
2023-09-12 20:37:41 -0300

    alterações 12/09

--- sistema de estoque.py
+++ sistema de estoque.py
@@ -15,6 +15,15 @@ def conect():
         database='dados_usuario',
     )
     cursor = conexao.cursor()
+def conect2():
+    global conexao, cursor
+    conexao = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='macaco123@3',
+        database='cadastroprodutos',
+    )
+    cursor = conexao.cursor()
 def titulo():
     print('*'*30)
     print('P.O.G.E.R.S')
@@ -24,7 +33,7 @@ def pular():
     print('*'*30)
 def imagem():
     
-    global imagem1,imagem2,imagem3,imagem4,imagem5,imagem6,imagem7,imagem8
+    global imagem1,imagem2,imagem3,imagem4,imagem5,imagem6,imagem7,imagem8,imagem9,imagem10
     imagem1=r'C:walpaper\tela_login.png'
     imagem2=r'C:walpaper\tela_home.png'
     imagem3=r'C:walpaper\tela_cadastro.png'
@@ -33,6 +42,8 @@ def imagem():
     imagem6=r'C:walpaper\tela_caixa.png'
     imagem7=r'C:walpaper\tela_config.png'
     imagem8=r'C:walpaper\tela_botao.png'
+    imagem9=r'C:walpaper\tela_excluir.png'
+    imagem10=r'C:walpaper\tela_cadprodutos.png'
 def main():
 #______________________________________________________MONTAGEM DA JANELA LOGIN_____________________________________________
     #criar a janela login
@@ -64,11 +75,16 @@ def main():
             main()
         # DEF VER ESTOQUE 
         def estoque():
+            conect2()
             global imagem4
-            janela2.destroy()
+        #________________________________________________MONTAGEM DA JANELA ESTOQUE__________________________________________________   
+            try:
+                janela2.destroy()
+            except:
+                print('janela já destruida')
             print( 'ESTOQUE')
             pular()
-        #________________________________________________MONTAGEM DA JANELA ESTOQUE__________________________________________________
+        
             #criar a janela de estoque 
             janela_e=tk.Tk()
             janela_e.title('Estoque')
@@ -76,8 +92,264 @@ def main():
             def voltar_estoque():
                 janela_e.destroy()
                 home()
+
+            def excluir():
+                global imagem9
+                janela_e.destroy()
+                print( 'EXCLUIR ESTOQUE')
+                pular()
+            #________________________________________________MONTAGEM DA JANELA EXCLUIR DE ESTOQUE____________________________________
+                #criar a janela de estoque 
+                janela_x=tk.Tk()
+                janela_x.title('Excluir estoque')
+             #___________________________________________________CRIANDO QUARTA DEF'S____________________________________________________
+                def voltar_estoquex():
+                    janela_x.destroy()
+                    estoque()
+                def excluir_produtos():
+                    codigo = entry_cod.get()
+                    if not codigo:
+                        messagebox.showerror("Erro", "O campo Código do Produto é obrigatório.")
+                        return
+
+                    comando = f"DELETE FROM cadastroprodutos WHERE codigoproduto = {codigo}"
+
+                    try:
+                        cursor.execute(comando)
+                        conexao.commit()
+                        messagebox.showinfo("Sucesso", "Produto excluído com sucesso!")
+                        
+                    except Exception as e:
+                        conexao.rollback()
+                        messagebox.showerror("Erro", f"Erro ao excluir o produto: {str(e)}")
+
+                #_____________________________________________________ADICIONANDO WIDGETS NA TELA EXCLUIR ESTOQUE___________________________
+                #carregando image 9
+                carregar_imagem9=Image.open(imagem9)
+                carregar_imagem9.thumbnail((max_width, max_height))
+                lendo_imagem9=ImageTk.PhotoImage(carregar_imagem9)
+
+                #CRIAR LABEL PRINCIPAL
+                lab9=tk.Label(janela_x, image=lendo_imagem9)
+                lab9.image=lendo_imagem9
+                lab9.pack()
+                
+                #CRIAR ENTRY'S
+                entry_cod=tk.Entry(janela_x,highlightthickness=0)
+                entry_cod.place( x=220, y=295)
+                print(f'O codigo cadastrado foi={entry_cod}')
+
+                entry_prodx=tk.Entry(janela_x,highlightthickness=0)
+                entry_prodx.place( x=150, y=200)
+                print(f'O produto excluido foi={entry_prodx}')
+
+                entry_categ=tk.Entry(janela_x,highlightthickness=0)
+                entry_categ.place( x=150, y=400)
+                print(f'A categoria excluida foi={entry_categ}')
+
+                #CRIANDO BUTTON'S
+                bot_voltare=tk.Button(janela_x, command=voltar_estoquex,text='< Voltar', borderwidth=0, bg='#7c14c4')
+                bot_voltare.place( x=880, y=30,width= 80, height=50 )
+
+                bot_excluir=tk.Button(janela_x, command=excluir_produtos,text='Excluir', borderwidth=0, bg='white')
+                bot_excluir.place( x=400, y=120,width=60, height=30 )
+           
             def adicionar_e():
-                titulo()
+                global imagem10
+                janela_e.destroy()
+                print( 'CADASTRAR ESTOQUE')
+                pular()
+            #________________________________________________MONTAGEM DA JANELA CADASTRO DE ESTOQUE____________________________________
+                #criar a janela de estoque 
+                janela_c=tk.Tk()
+                janela_c.title('Cadastrar estoque')
+            #___________________________________________________CRIANDO QUARTA DEF'S____________________________________________________
+                def voltar_estoquec():
+                    janela_c.destroy()
+                    estoque()
+
+                def cadastrar_p():
+                    codigo=entry_cod.get()
+                    print(codigo)
+                    produto=entry_prod.get()
+                    prec_c=float(entry_precc.get())
+                    prec_v=float(entry_precov.get())
+                    quant=int(entry_quant.get())
+                    categ=entry_categ.get()
+                    
+                    try:
+                    #ADICIONAR BANCO DE DADOS
+                        comando= f'INSERT INTO cadastroprodutos(codigoproduto,produto,precocompra,precovenda,quantidade,categoria) VALUES("{codigo}","{produto}",{prec_c},{prec_v},{quant},"{categ}")'
+                        cursor.execute(comando) 
+                        conexao.commit()
+                        messagebox.showinfo("Sucesso", "Produto cadastrado com sucesso!")
+                        
+                    except Exception as e:
+                        conexao.rollback()
+                        messagebox.showerror("Erro", f"Erro ao cadastrar o produto: {str(e)}")
+
+            #_____________________________________________________ADICIONANDO WIDGETS NA TELA CADASTRAR ESTOQUE___________________________________________
+                    #carregando image 10
+                    carregar_imagem10=Image.open(imagem10)
+                    carregar_imagem10.thumbnail((max_width, max_height))
+                    lendo_imagem10=ImageTk.PhotoImage(carregar_imagem10)
+
+                    #CRIAR LABEL PRINCIPAL
+                    lab10=tk.Label(janela_c, image=lendo_imagem10)
+                    lab10.image=lendo_imagem10
+                    lab10.pack()
+
+             #_____________________________________________________ADICIONANDO WIDGETS NA TELA CADASTRAR ESTOQUE___________________________
+             #carregando image 10
+                carregar_imagem10=Image.open(imagem10)
+                carregar_imagem10.thumbnail((max_width, max_height))
+                lendo_imagem10=ImageTk.PhotoImage(carregar_imagem10)
+
+                #CRIAR LABEL PRINCIPAL
+                lab10=tk.Label(janela_c, image=lendo_imagem10)
+                lab10.image=lendo_imagem10
+                lab10.pack()
+
+                #CRIAR ENTRY'S
+                entry_cod=tk.Entry(janela_c,highlightthickness=0)
+                entry_cod.place( x=560, y=200)
+                print(f'O codigo cadastrado foi={entry_cod}')
+
+                entry_prod=tk.Entry(janela_c,highlightthickness=0)
+                entry_prod.place( x=150, y=200)
+                print(f'O produto cadastrado foi={entry_prod}')
+
+                entry_precc=tk.Entry(janela_c,highlightthickness=0)
+                entry_precc.place( x=210, y=300)
+                print(f'O preço cadastrado foi={entry_precc}')
+
+                entry_precov=tk.Entry(janela_c,highlightthickness=0)
+                entry_precov.place( x=560, y=300)
+                print(f'O preço de venda cadastrado foi={entry_precov}')
+
+                entry_quant=tk.Entry(janela_c,highlightthickness=0)
+                entry_quant.place( x=560, y=400)
+                print(f'O quantidade cadastrado foi={entry_quant}')
+
+                entry_categ=tk.Entry(janela_c,highlightthickness=0)
+                entry_categ.place( x=150, y=400)
+                print(f'O categoria cadastrado foi={entry_categ}')
+
+                #CRIANDO BUTTON'S
+                bot_voltare=tk.Button(janela_c, command=voltar_estoquec,text='< Voltar', borderwidth=0, bg='#7c14c4')
+                bot_voltare.place( x=880, y=30,width= 80, height=50 )
+
+                bot_salvar=tk.Button(janela_c, command=cadastrar_p,text='salvar', borderwidth=0, bg='white')
+                bot_salvar.place( x=780, y=115,width= 70, height=40 )
+        
+            def atualizar():
+                global imagem10
+                janela_e.destroy()
+                print( 'ATUALIZAR ESTOQUE')
+                pular()
+            #________________________________________________MONTAGEM DA JANELA CADASTRO DE ESTOQUE____________________________________
+                #criar a janela de estoque 
+                janela_a=tk.Tk()
+                janela_a.title('Atualizar estoque')
+            #___________________________________________________CRIANDO QUARTA DEF'S____________________________________________________
+                def atualizar_a():
+                    codigo=entry_cod.get()
+                    if not codigo:
+                        messagebox.showerror("Erro", "O campo Código do Produto é obrigatório.")
+                        return
+
+                    produto = entry_prod.get() if entry_prod.get() else None
+                    preco_v = entry_precov.get() if entry_precov.get() else None
+                    preco_c = entry_precc.get() if entry_precc.get() else None
+                    quant = entry_quant.get() if entry_quant.get() else None
+                    categ = entry_categ.get() if entry_categ.get() else None
+                
+                    try:
+                        if quant:
+                            qtd = int(quant)  
+                        else:
+                            qtd = None
+
+                        comando = f"UPDATE cadastroprodutos SET "
+                        if produto:
+                            comando += f"produto = '{produto}', "
+                        if preco_v:
+                            comando += f"precovenda = {preco_v}, "
+                        if preco_c:
+                            comando += f"precocompra = {preco_c}, "
+                        if qtd is not None:
+                            comando += f"quantidade = {qtd}, "
+                        if categ:
+                            comando += f"categoria = '{categ}', "
+
+                        comando = comando.rstrip(", ")
+
+                        comando += f" WHERE codigoproduto = {codigo}"
+
+                        cursor.execute(comando)
+                        conexao.commit()
+
+                        entry_prod.delete(0, tk.END)
+                        entry_cod.delete(0, tk.END)
+                        entry_precov.delete(0, tk.END)
+                        entry_precc.delete(0, tk.END)
+                        entry_quant.delete(0, tk.END)
+                        entry_categ.delete(0, tk.END)
+
+                        messagebox.showinfo("Sucesso", "Produto atualizado com sucesso!")
+
+                    except Exception as e:
+                        conexao.rollback()
+                        messagebox.showerror("Erro", f"Erro ao atualizar o produto: {str(e)}")
+
+
+                def voltar_estoquea():
+                    janela_a.destroy()
+                    estoque()
+
+            #_____________________________________________________ADICIONANDO WIDGETS NA TELA CADASTRAR ESTOQUE___________________________
+             #carregando image 10
+                carregar_imagem10=Image.open(imagem10)
+                carregar_imagem10.thumbnail((max_width, max_height))
+                lendo_imagem10=ImageTk.PhotoImage(carregar_imagem10)
+
+                #CRIAR LABEL PRINCIPAL
+                lab10=tk.Label(janela_a, image=lendo_imagem10)
+                lab10.image=lendo_imagem10
+                lab10.pack()
+
+                #CRIAR ENTRY'S
+                entry_cod=tk.Entry(janela_a,highlightthickness=0)
+                entry_cod.place( x=560, y=200)
+                print(f'O codigo atualizado foi={entry_cod}')
+
+                entry_prod=tk.Entry(janela_a,highlightthickness=0)
+                entry_prod.place( x=150, y=200)
+                print(f'O produto atualizad foi={entry_prod}')
+
+                entry_precc=tk.Entry(janela_a,highlightthickness=0)
+                entry_precc.place( x=210, y=300)
+                print(f'O preço atualizar foi={entry_precc}')
+
+                entry_precov=tk.Entry(janela_a,highlightthickness=0)
+                entry_precov.place( x=560, y=300)
+                print(f'O preço de venda atualizado foi={entry_precov}')
+
+                entry_quant=tk.Entry(janela_a,highlightthickness=0)
+                entry_quant.place( x=560, y=400)
+                print(f'O quantidade atualizado foi={entry_quant}')
+
+                entry_categ=tk.Entry(janela_a,highlightthickness=0)
+                entry_categ.place( x=150, y=400)
+                print(f'O categoria atualizardofoi={entry_categ}')
+
+                #CRIANDO BUTTON'S
+                bot_voltare=tk.Button(janela_a, command=voltar_estoquea,text='< Voltar', borderwidth=0, bg='#7c14c4')
+                bot_voltare.place( x=880, y=30,width= 80, height=50 )
+
+                bot_salvar=tk.Button(janela_a, command=atualizar_a,text='Atualizar', borderwidth=0, bg='white')
+                bot_salvar.place( x=780, y=115,width= 70, height=40 )
+
          #_____________________________________________________ADICIONANDO WIDGETS NA TELA ESTOQUE___________________________________________
             #carregando image 4
             carregar_imagem4=Image.open(imagem4)
@@ -91,7 +363,16 @@ def main():
 
             #CRIANDO BUTTON'S
             bot_voltare=tk.Button(janela_e, command=voltar_estoque,text='< Voltar', borderwidth=0, bg='#7c14c4')
-            bot_voltare.place( x=890, y=20,width= 90, height=50 )
+            bot_voltare.place( x=880, y=25,width=60, height=30 )
+
+            bot_excluir=tk.Button(janela_e, command=excluir,text='Excluir', borderwidth=0, bg='#ea080e')
+            bot_excluir.place( x=750, y=68,width= 60, height=30 )
+            
+            bot_adicionar=tk.Button(janela_e, command=adicionar_e,text='Adicionar', borderwidth=0, bg='#2b4b05')
+            bot_adicionar.place( x=630, y=68,width=60, height=30 )
+
+            bot_atualizar=tk.Button(janela_e, command=atualizar,text='Atualizar', borderwidth=0, bg='#19b5e5')
+            bot_atualizar.place( x=880, y=68,width=60, height=30 )
         #DEF PDV
         def vendas():
             global imagem6
@@ -164,6 +445,8 @@ def main():
             def voltar_loging():
                 janela_g.destroy()
                 home()
+            def atualizar_dados():
+                titulo()
         #_____________________________________________________ADICIONANDO WIDGETS NA TELA CONFIGURAÇÃO _____________________________________________
             #carregando image 7
             carregar_imagem7=Image.open(imagem7)
@@ -178,6 +461,22 @@ def main():
              #CRIAR BUTTON'S
             bot_voltarv=tk.Button(janela_g, command=voltar_loging,text='< Voltar', borderwidth=0, bg='#7c14c4')
             bot_voltarv.place( x=907, y=13,width= 50, height=50 )
+
+            bot_conta=tk.Button(janela_g)
+            bot_conta.place()
+
+            bot_gestao=tk.Button(janela_g)
+            bot_gestao.place()
+
+            bot_suporte=tk.Button(janela_g)
+            bot_suporte.place()
+
+            bot_not=tk.Button(janela_g)
+            bot_not.place()
+
+            bot_relat=tk.Button(janela_g)
+            bot_relat.place()
+
     #______________________________________________________ADICIONANDO WIDGETS NA TELA HOME_________________________________________
         #carregando image 2
         carregar_imagem2=Image.open(imagem2)
